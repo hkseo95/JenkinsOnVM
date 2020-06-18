6539e394a75938d7f3d5a194efca5802118ef562
Heekyoung Seo <hkseo@hanyang.ac.kr>
2020-06-18 16:31:05 +0900

    Update index.js
    test222

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I love LINA on EC2');
+    res.send('I love LINA222 on EC2');
 });
 
 app.get('/start', function(req, res){
